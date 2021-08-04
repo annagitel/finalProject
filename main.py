0f7619fbf0afc3e9ee593715846e9ccd660e6240
Anna Sandler <asandler@redhat.com>
2021-08-04 14:06:15 -0400

    django implementation

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+"""
+this python script runs a rapid PT test for a given API and displays the resaults in a dashboard that opens automaticaly
+"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def dos_run():
+    pass
+
+def auth_abuse_run():
+    pass
+
+def sql_injection_run():
+    pass
+
+def xss_injection_run():
+    pass
+
+def mitm_run():
+    pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
