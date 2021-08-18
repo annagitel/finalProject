9b40727a27306e20d49575479c7ed4d5aa034017
Anna Sandler <asandler@redhat.com>
2021-08-18 12:57:03 -0400

    added files

--- main.py
+++ main.py
@@ -2,20 +2,5 @@
 this python script runs a rapid PT test for a given API and displays the resaults in a dashboard that opens automaticaly
 """
 
-def dos_run():
-    pass
-
-def auth_abuse_run():
-    pass
-
-def sql_injection_run():
-    pass
-
-def xss_injection_run():
-    pass
-
-def mitm_run():
-    pass
-
-
 if __name__ == '__main__':
+    pass
