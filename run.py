0295b174dd6d47aa3df6dbbf16627909a6877eef
Anna Sandler <asandler@redhat.com>
2021-09-12 01:17:42 -0400

    infra changes

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ import json
 
 from flask import Flask, render_template
 
+import testBRTFRC
+import testDOS
+import testJSONJCTN
+import testXSS
 from test_resaults import TestRes
 
 app = Flask(__name__)
@@ -17,36 +21,33 @@ app = Flask(__name__)
 def dashboard():
     data = {}
 
-    brt1 = TestRes("", testBRTFRC.blabla, "").writeToJson()
-    brt2 = TestRes("", testBRTFRC.blabla, "").writeToJson()
-    brt3 = TestRes("", testBRTFRC.blabla, "").writeToJson()
-    brt4 = TestRes("", testBRTFRC.blabla, "").writeToJson()
+    brt1 = TestRes("bruteforce simple", testBRTFRC.simple_brtfrc(), "got into system").writeToJson()
+    brt2 = TestRes("bruteforce dictionary", testBRTFRC.rand(), "passed dictionary").writeToJson()
+    brt3 = TestRes("bruteforce random", testBRTFRC.dict(), "Did not block").writeToJson()
 
-    brt_list = [brt1, brt2, brt3, brt4]
+    brt_list = [brt1, brt2, brt3]
     data["Authentication Bruteforce"] = brt_list
 
-    dos1 = TestRes("", testDOS.blabla, "").writeToJson()
-    dos2 = TestRes("", testDOS.blabla, "").writeToJson()
-    dos3 = TestRes("", testDOS.blabla, "").writeToJson()
-    dos4 = TestRes("", testDOS.blabla, "").writeToJson()
+    dos1 = TestRes("DoS simple", testDOS.testDOS(), "did not block").writeToJson()
+    dos2 = TestRes("Dos spoofed ip", testDOS.testDOS(spoofIP=True), "did not block").writeToJson()
+    dos3 = TestRes("DoS random queries", testDOS.testDOS(randomQueries=True), "did not block").writeToJson()
+    dos4 = TestRes("DoS spoofed ip + random", testDOS.testDOS(spoofIP=True, randomQueries=True), "did not block").writeToJson()
 
     dos_list = [dos1, dos2, dos3, dos4]
     data["Denial Of Service"] = dos_list
 
-    xss1 = TestRes("", testXSS.blabla, "").writeToJson()
-    xss2 = TestRes("", testXSS.blabla, "").writeToJson()
-    xss3 = TestRes("", testXSS.blabla, "").writeToJson()
-    xss4 = TestRes("", testXSS.blabla, "").writeToJson()
+    xss1 = TestRes("simple redirects", testXSS.simple_redirect(), "redirected!").writeToJson()
+    xss2 = TestRes("write data", testXSS.write_data(), "changed page data").writeToJson()
+    xss3 = TestRes("delete data", testXSS.delete_data(), "data was deleted from page").writeToJson()
 
-    xss_list = [xss1, xss2, xss3, xss4]
+    xss_list = [xss1, xss2, xss3]
     data["Xross Site Scripting"] = xss_list
 
-    inj1 = TestRes("", testJSONJCTN.blabla, "").writeToJson()
-    inj2 = TestRes("", testJSONJCTN.blabla, "").writeToJson()
-    inj3 = TestRes("", testJSONJCTN.blabla, "").writeToJson()
-    inj4 = TestRes("", testJSONJCTN.blabla, "").writeToJson()
+    inj1 = TestRes("simple string", testJSONJCTN.simple_string(), "was not detected").writeToJson()
+    inj2 = TestRes("get data", testJSONJCTN.insert_data(), "returned data").writeToJson()
+    inj3 = TestRes("insert data", testJSONJCTN.get_pass(), "file was changed").writeToJson()
 
-    inj_list = [inj1, inj2, inj3, inj4]
+    inj_list = [inj1, inj2, inj3]
     data["JSON injection"] = inj_list
 
     with open('data.json', 'r') as f:
